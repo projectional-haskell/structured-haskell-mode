238cc1cfb19f22d39c968d6e2bab2343ca333c12
external-reality <ecjones2040@gmail.com>
2014-01-31 02:53:22 -0500

    General readability refactoring

--- src/Main.hs
+++ src/Main.hs
@@ -6,142 +6,125 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE ExistentialQuantification #-}
 
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 -- | Take in Haskell code and output a vector of source spans and
 -- their associated node type and case.
 
 module Main (main) where
 
 import Control.Applicative
-import Data.Char
 import Data.Data
 import Data.Maybe
 import Language.Haskell.Exts.Annotated
-import Language.Haskell.Exts.Annotated.Fixity
 import System.Environment
-import System.IO
 
-type Parser = ParseMode -> String -> ParseResult D
+data TopLevelSrcContainer = forall ast. Data ast => TopLevelSrcContainer ast
 
-data D = forall a. Data a => D a
+type Parser = ParseMode -> String -> ParseResult TopLevelSrcContainer
 
--- | Main entry point.
+instance Alternative ParseResult where
+  empty = ParseFailed  undefined undefined
+  ParseFailed{} <|> x = x
+  x <|> _             = x
+               
 main :: IO ()
 main = do
   code <- getContents
   action:typ:_ <- getArgs
   outputWith action typ code
-
+  
 outputWith :: String -> String -> String -> IO ()
-outputWith action typ code =
-  case typ of
-    "decl" ->
-      output action
-             (\mode code ->
-                fmap D (fmap fix (parseDeclWithMode mode code)) <|>
-                fmap D (parseImport mode code) <|>
-                fmap D (fmap fix (parseModuleWithMode mode code)) <|>
-                fmap D (parseModulePragma mode code))
-             code
-    _ -> error "Unknown parser type."
-  where fix ast = fromMaybe ast (applyFixities baseFixities ast)
-
-instance Alternative ParseResult where
-  empty = ParseFailed undefined undefined
-  ParseFailed{} <|> x = x
-  x <|> _             = x
-
--- | Get the type of the parser.
-parserRep :: Typeable ast => ast -> String
-parserRep = show . head . typeRepArgs . typeOf
+outputWith action typ code = case typ of
+  "decl" -> output action parseTopLevelElement code
+  _ -> error "Unknown parser type."
 
 -- | Output AST info for the given Haskell code.
 output :: String -> Parser -> String -> IO ()
-output action parseWithMode code = do
-  case parseWithMode parseMode code of
-    ParseFailed _ e -> error e
-    ParseOk (D ast) -> case action of
-        "check" -> return ()
-        "parse" -> putStrLn ("[" ++ concat (genHSE ast) ++ "]")
-        _       -> error "unknown action"
+output action parser code = case parser parseMode code of
+  ParseFailed _ e -> error e
+  ParseOk (TopLevelSrcContainer ast) -> case action of
+    "check" -> return ()
+    "parse" -> putStrLn ("[" ++ concat (genHSE ast) ++ "]")
+    _       -> error "unknown action"
+
+parseTopLevelElement :: ParseMode -> String -> ParseResult TopLevelSrcContainer
+parseTopLevelElement mode code =
+  TopLevelSrcContainer . fix <$> parseDeclWithMode mode code   <|>
+  TopLevelSrcContainer       <$> parseImport mode code         <|>
+  TopLevelSrcContainer . fix <$> parseModuleWithMode mode code <|>
+  TopLevelSrcContainer       <$> parseModulePragma mode code
+
+fix :: AppFixity ast => ast SrcSpanInfo -> ast SrcSpanInfo
+fix ast = fromMaybe ast (applyFixities baseFixities ast)
+    
+-- | Generate a list of spans from the HSE AST.
+genHSE :: Data a => a -> [String]
+genHSE x = case gmapQ TopLevelSrcContainer x of
+  zs@(TopLevelSrcContainer y:ys) -> case cast y of
+    Just s -> spanHSE (show (show (typeOf x)))
+                      (showConstr (toConstr x))
+                      (srcInfoSpan s) :
+                        concatMap (\(i,TopLevelSrcContainer d) -> pre x i ++ genHSE d)
+                                  (zip [0..] ys)
+    _      -> concatMap (\(TopLevelSrcContainer d) -> genHSE d) zs
+  _ -> []
 
 -- | Parse mode, includes all extensions, doesn't assume any fixities.
 parseMode :: ParseMode
-parseMode =
-  defaultParseMode { extensions = allExtensions
-                   , fixities   = Nothing
-                   }
-  where allExtensions =
-          filter (\x ->
-                   case x of
-                     DisableExtension x -> False
-                     _ -> True)
-                 knownExtensions
-
--- | Generate a list of spans from the HSE AST.
-genHSE :: Data a => a -> [String]
-genHSE x =
-  case gmapQ D x of
-    zs@(D y:ys) ->
-      case cast y of
-        Just s ->
-          spanHSE (show (show (typeOf x)))
-                  (showConstr (toConstr x))
-                  (srcInfoSpan s) :
-          concat (map (\(i,(D d)) -> pre x i ++ genHSE d)
-                      (zip [0..] ys))
-        _ ->
-          concatMap (\(D d) -> genHSE d) zs
-    _ -> []
+parseMode = defaultParseMode { extensions = allExtensions
+                             , fixities   = Nothing
+                             }
+ where allExtensions = filter isDisabledExtention knownExtensions
+       isDisabledExtention (DisableExtension _) = False
+       isDisabledExtention _                    = True
 
 -- | Pre-children tweaks for a given parent at index i.
 --
 pre :: (Typeable a) => a -> Integer -> [String]
-pre x i =
-  case cast x of
-    -- <foo { <foo = 1> }> becomes <foo <{ <foo = 1> }>>
-    Just (RecUpdate SrcSpanInfo{srcInfoPoints=(start:_),srcInfoSpan=end} _ _)
-      | i == 1 ->
-        [spanHSE (show "RecUpdates")
-                 "RecUpdates"
-                 (SrcSpan (srcSpanFilename start)
-                          (srcSpanStartLine start)
-                          (srcSpanStartColumn start)
-                          (srcSpanEndLine end)
-                          (srcSpanEndColumn end))]
-    _ -> case cast x :: Maybe (Deriving SrcSpanInfo)  of
-           -- <deriving (X,Y,Z)> becomes <deriving (<X,Y,Z>)
-           Just (Deriving _ ds@(_:_)) ->
-             [spanHSE (show "InstHeads")
-                      "InstHeads"
-                      (SrcSpan (srcSpanFilename start)
-                               (srcSpanStartLine start)
-                               (srcSpanStartColumn start)
-                               (srcSpanEndLine end)
-                               (srcSpanEndColumn end))
-             |Just (IHead (SrcSpanInfo start _) _ _) <- [listToMaybe ds]
-             ,Just (IHead (SrcSpanInfo end _) _ _) <- [listToMaybe (reverse ds)]]
-           _ -> []
+pre x i = case cast x of
+  -- <foo { <foo = 1> }> becomes <foo <{ <foo = 1> }>>
+  Just (RecUpdate SrcSpanInfo{srcInfoPoints=(start:_),srcInfoSpan=end} _ _)
+       | i == 1 ->
+         [spanHSE (show "RecUpdates")
+                  "RecUpdates"
+                  (SrcSpan (srcSpanFilename start)
+                           (srcSpanStartLine start)
+                           (srcSpanStartColumn start)
+                           (srcSpanEndLine end)
+                           (srcSpanEndColumn end))]
+  _ -> case cast x :: Maybe (Deriving SrcSpanInfo)  of
+         -- <deriving (X,Y,Z)> becomes <deriving (<X,Y,Z>)
+         Just (Deriving _ ds@(_:_)) ->
+           [spanHSE (show "InstHeads")
+                    "InstHeads"
+                    (SrcSpan (srcSpanFilename start)
+                             (srcSpanStartLine start)
+                             (srcSpanStartColumn start)
+                             (srcSpanEndLine end)
+                             (srcSpanEndColumn end))
+           | Just (IHead (SrcSpanInfo start _) _ _) <- [listToMaybe ds]
+           , Just (IHead (SrcSpanInfo end _) _ _) <- [listToMaybe (reverse ds)]]
+         _ -> []
 
 -- | Generate a span from a HSE SrcSpan.
 spanHSE :: String -> String -> SrcSpan -> String
-spanHSE typ cons (SrcSpan _ a b c d) =
-  concat ["["
-         ,unwords [unqualify typ
-                  ,cons
-                  ,show a
-                  ,show b
-                  ,show c
-                  ,show d]
-         ,"]"]
-  where unqualify = go [] where
-          go acc ('.':cs) = go [] cs
-          go acc (c:cs)   = go (c:acc) cs
-          go acc []       = reverse acc
+spanHSE typ cons SrcSpan{..} = "[" ++ spanStr ++ "]"
+  where unqualify = dropUntilLastOccurance '.'
+        spanStr   = unwords [ unqualify typ
+                            , cons
+                            , show srcSpanStartLine
+                            , show srcSpanStartColumn
+                            , show srcSpanEndLine
+                            , show srcSpanStartColumn
+                            ]
 
--- | Pretty print a source location.
-printSrcLoc :: SrcLoc -> String
-printSrcLoc SrcLoc{..} =
-  srcFilename ++ ":" ++ show srcLine ++ ":" ++ show srcColumn
+dropUntilLastOccurance :: Eq a => a -> [a] -> [a]
+dropUntilLastOccurance ch = go []
+  where
+    go _ (c:cs) | c == ch = go [] cs
+    go acc (c:cs)         = go (c:acc) cs
+    go acc []             = reverse acc
 
 --------------------------------------------------------------------------------
 -- Parsers that HSE hackage doesn't have
